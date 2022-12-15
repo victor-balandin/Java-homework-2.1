ba63e5b396a1ff8f28fa7d7e1a82eb699742ebca
Victor Balandin <eparis@atlassian.com>
2022-12-15 13:11:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 7777;
+        int costPerMile = 20;
+        System.out.println("Начислено миль - " + price / costPerMile);
+    }
+}
