e7839d505ebef05c0e30222a8065aa3e2b36080f
Victor Balandin <eparis@atlassian.com>
2022-12-20 13:12:09 +0300

    Homework 3.1

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate (int cost){
+        int costPerMile = 20;
+        int result = cost / costPerMile;
+        return result;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 7777;
-        int costPerMile = 20;
-        System.out.println("Начислено миль - " + price / costPerMile);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
